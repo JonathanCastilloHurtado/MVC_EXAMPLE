8dc80b564f26c605c728ae4714616ed8c578642b
Jonathan Castillo <developer.jonathanc@gmail.com>
2019-08-16 11:45:34 -0500

    some corrections

--- app/src/main/java/com/example/mvc_example/MainActivity.java
+++ app/src/main/java/com/example/mvc_example/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ProgressDialog progressDialog;
     Controller controller;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
